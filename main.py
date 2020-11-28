89036c58bed1e049941e6ebc977e26c6155c33f1
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2020-11-28 18:10:19 +0300

    add ugly background and doodle image

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ kivy.require('1.10.1')
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+from widgets.playground import Playground
+from widgets.doodle import Doodle
 
-class Playground(Widget):
-    pass
-
-
-class DoodleJump(App):
+class DoodleApp(App):
     def build(self):
         return Playground()
 
 
 if __name__ == '__main__':
-    DoodleJump().run()
+    DoodleApp().run()
