3457ab4540df15e217ec14fe4cd988de974e7b21
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2020-11-29 13:14:42 +0300

    add ability to teleport between ends of screen

--- widgets/doodle.py
+++ widgets/doodle.py
@@ -10,15 +10,32 @@ class Doodle(Widget):
     doodle_img = ObjectProperty(None)
     
     def move_right(self):
-        anim = Animation(x = Window.size[0] + self.width/2, t='linear', duration=.5)
+        duration = 0.4 * (Window.size[0] - self.pos[0]) / Window.size[0]
+        anim = Animation(x = Window.size[0], t='linear', d=duration)
         anim.start(self)
         anim.start(self.doodle_img)
     
     def move_left(self):
-        anim = Animation(x = -self.width/2, t='linear', duration=.5)
+        duration = 0.4 * self.pos[0] / Window.size[0]
+        anim = Animation(x = -self.width, t='linear', d=duration)
         anim.start(self)
         anim.start(self.doodle_img)
     
     def move_stop(self):
         Animation.stop_all(self, 'x')
         Animation.stop_all(self.doodle_img, 'x')
+    
+    def update(self, *args):
+        if self.pos[0] >= Window.size[0] - self.width/2:
+            self.pos[0] = -self.width/2 + 1
+            self.doodle_img.pos[0] = -self.width/2 + 1
+            
+            self.move_stop()
+            self.move_right()
+        elif self.pos[0] <= -self.width/2:
+            self.pos[0] = Window.size[0] - self.width/2 - 1
+            self.doodle_img.pos[0] = Window.size[0] - self.width/2 - 1
+            
+            self.move_stop()
+            self.move_left()
+    
--- widgets/playground.py
+++ widgets/playground.py
@@ -4,11 +4,13 @@ kivy.require('1.10.1')
 from kivy.uix.widget import Widget
 from kivy.vector import Vector
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 
 class Playground(Widget):
     doodle = ObjectProperty(None)
     
     def start(self):
+        Clock.schedule_interval(self.doodle.update, 0.001)
         print("aaaaaaaaaa, I'm alive!!!")
     
     def on_touch_down(self, touch):
