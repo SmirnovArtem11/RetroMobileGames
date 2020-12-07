5ea4b8551641a9eeb0b9817ea5b86f8ea9cc9234
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2020-12-07 17:47:35 +0300

    init project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+from sprites.player import Player
+
 WIDTH = 414
 HEIGHT = 736
 FPS = 60
@@ -13,14 +15,23 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Doodle Jump")
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
 running = True
+
 while running:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    screen.fill(BLACK)
-    pygame.display.flip()
+  clock.tick(FPS)
+  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+  
+  all_sprites.update()
+  
+  screen.fill(BLACK)
+  all_sprites.draw(screen)
+  pygame.display.flip()
 
 pygame.quit()
